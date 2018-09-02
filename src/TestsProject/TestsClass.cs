90279733d29c191b0cf60017e631bb82ec6acc49
Alexandre Rabérin <kernelith@live.fr>
2018-09-02 02:01:07 +0200

    Add support for older frameworks and property in TestClass.

--- src/TestsProject/TestsClass.cs
+++ src/TestsProject/TestsClass.cs
@@ -6,5 +6,23 @@
     public class TestClass
     {
         public string Test { get; set; }
+
+#if NET35
+        public string Property35 { get; set; }
+#elif NET40
+        public string Property40 { get; set; }
+#elif NET45
+        public string Property45 { get; set; }
+#elif NET452
+        public string Property452 { get; set; }
+#elif NETSTANDARD1_1
+        public string PropertyStandard11 { get; set; }
+#elif NETCOREAPP1_0
+        public string PropertyCore1 { get; set; }
+#elif NETCOREAPP2_0
+        public string PropertyCore2 { get; set; }
+#else
+        public string PropertyOther { get; set; }
+#endif
     }
 }
